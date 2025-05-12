f88e9bb226b78b5757dbfd4f508ce6d7b3fc0fa6
sonlqt <ojtplf@sendo.vn>
2025-05-12 14:02:22 +0700

    SE184097_LeQuangThaiSon_Lab1

---
+++ PharmacyConsoleApp/Models/Lab1PharmaceuticalDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyConsoleApp.Models;
+
+public partial class Lab1PharmaceuticalDbContext : DbContext
+{
+    public Lab1PharmaceuticalDbContext()
+    {
+    }
+
+    public Lab1PharmaceuticalDbContext(DbContextOptions<Lab1PharmaceuticalDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+
+    public virtual DbSet<MedicineInformation> MedicineInformations { get; set; }
+
+    public virtual DbSet<StoreAccount> StoreAccounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=1234567890;Database=Lab1PharmaceuticalDB;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Manufacturer>(entity =>
+        {
+            entity.HasKey(e => e.ManufacturerId).HasName("PK__Manufact__357E5CA12FB3A54F");
+
+            entity.ToTable("Manufacturer");
+
+            entity.Property(e => e.ManufacturerId)
+                .HasMaxLength(20)
+                .HasColumnName("ManufacturerID");
+            entity.Property(e => e.ContactInformation).HasMaxLength(120);
+            entity.Property(e => e.CountryofOrigin).HasMaxLength(250);
+            entity.Property(e => e.ManufacturerName).HasMaxLength(100);
+            entity.Property(e => e.ShortDescription).HasMaxLength(400);
+        });
+
+        modelBuilder.Entity<MedicineInformation>(entity =>
+        {
+            entity.HasKey(e => e.MedicineId).HasName("PK__Medicine__4F2128F03F2EF155");
+
+            entity.ToTable("MedicineInformation");
+
+            entity.Property(e => e.MedicineId)
+                .HasMaxLength(30)
+                .HasColumnName("MedicineID");
+            entity.Property(e => e.ActiveIngredients).HasMaxLength(200);
+            entity.Property(e => e.DosageForm).HasMaxLength(400);
+            entity.Property(e => e.ExpirationDate).HasMaxLength(120);
+            entity.Property(e => e.ManufacturerId)
+                .HasMaxLength(20)
+                .HasColumnName("ManufacturerID");
+            entity.Property(e => e.MedicineName).HasMaxLength(160);
+            entity.Property(e => e.WarningsAndPrecautions).HasMaxLength(400);
+
+            entity.HasOne(d => d.Manufacturer).WithMany(p => p.MedicineInformations)
+                .HasForeignKey(d => d.ManufacturerId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__MedicineI__Manuf__3B75D760");
+        });
+
+        modelBuilder.Entity<StoreAccount>(entity =>
+        {
+            entity.HasKey(e => e.StoreAccountId).HasName("PK__StoreAcc__42D52A6A2BC40E63");
+
+            entity.ToTable("StoreAccount");
+
+            entity.HasIndex(e => e.EmailAddress, "UQ__StoreAcc__49A147401DE83BF2").IsUnique();
+
+            entity.Property(e => e.StoreAccountId)
+                .ValueGeneratedNever()
+                .HasColumnName("StoreAccountID");
+            entity.Property(e => e.EmailAddress).HasMaxLength(90);
+            entity.Property(e => e.StoreAccountDescription).HasMaxLength(140);
+            entity.Property(e => e.StoreAccountPassword).HasMaxLength(90);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
